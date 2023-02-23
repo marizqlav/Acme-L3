5784760b08a440251f25585b06fba7f2d6a94400
JMGarCas <jmgarciacasillas135@gmail.com>
2023-02-23 17:45:55 +0100

    Add: Note (author missing)

---
+++ Acme-L3-D02/src/main/java/acme/entities/Note.java
@@ -0,0 +1,68 @@
+/*
+ * Consumer.java
+ *
+ * Copyright (C) 2012-2023 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Note extends AbstractRole {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date				instatiationMoment;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			author;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			message;
+
+	@Email
+	protected String			email;
+
+	@URL
+	protected String			moreInfo;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
