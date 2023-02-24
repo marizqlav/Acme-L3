ab5e70b68713c7f8ff8e470ab9ac6fbdd6fe2247
enrcabmun <enrcabmun@alum.us.es>
2023-02-24 01:01:14 +0100

    add: course entity

---
+++ Acme-L3-D02/src/main/java/acme/entities/Course.java
@@ -0,0 +1,63 @@
+/*
+ * UserIdentity.java
+ *
+ * Copyright (C) 2012-2023 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.PositiveOrZero;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Course extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}[0-9]{4}")
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			abstractt;
+
+	@NotNull
+	protected Boolean			theory;
+	//If 1, we're talking about a theory course. If 0, it's a hands-on course.
+	//This has to be fixed later on since we need to relationate it with a future entity
+	//Needs a constraint on purely theoretical courses
+
+	@NotNull
+	@PositiveOrZero
+	protected Double			price;
+
+	@URL
+	protected String			moreInfo;
+}
