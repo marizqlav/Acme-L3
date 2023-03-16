9e4de258c16266086881f53e9c6743ab1cc862b0
FelixoGudiel <felixangelgudiel@gmail.com>
2023-03-16 11:38:54 +0100

    doc: all reports

--- Acme-L3-D02/src/main/java/acme/entities/Enrolment.java
+++ Acme-L3-D02/src/main/java/acme/entities/Enrolment.java
@@ -38,7 +38,7 @@ public class Enrolment extends AbstractEntity {
 
 	@NotBlank
 	@Column(unique = true)
-	@Pattern(regexp = "[A-Z]{1,3}[0-9]{3}")
+	@Pattern(regexp = "^[A-Z]{1,3}[0-9]{3}$", message = "{regvalidation.code}")
 	protected String			code;
 
 	@NotBlank
