059c7dd6505fd0bfee60a30fa15989b80f34898b
JMGarCas <jmgarciacasillas135@gmail.com>
2023-03-16 16:26:14 +0100

    Improvement of assistant.csv and tutorial.csv and fix regex Tutorial

--- Acme-L3-D02/src/main/java/acme/entities/Tutorial.java
+++ Acme-L3-D02/src/main/java/acme/entities/Tutorial.java
@@ -29,7 +29,7 @@ public class Tutorial extends AbstractEntity {
 
 	@NotBlank
 	@Column(unique = true)
-	@Pattern(regexp = "[A-Z]{1,3}[0-9]{3}")
+	@Pattern(regexp = "^[A-Z]{1,3}[0-9]{3}$", message = "{regvalidation.code}")
 	protected String			code;
 
 	@NotBlank
