318eb4cc46a7124218d8aa0475d1a2de0cc6eee7
FelixoGudiel <felixangelgudiel@gmail.com>
2023-03-30 10:06:11 +0200

    review fixes

--- Acme-L3-D02/src/main/java/acme/features/student/course/StudentCourseRepository.java
+++ Acme-L3-D02/src/main/java/acme/features/student/course/StudentCourseRepository.java
@@ -15,7 +15,7 @@ public interface StudentCourseRepository extends AbstractRepository {
 	@Query("select c from Course c where c.id = :id")
 	Course findCourseById(int id);
 
-	@Query("select c from Course c")
+	@Query("select c from Course c where c.draftMode = false")
 	Collection<Course> findAllCourses();
 
 }
