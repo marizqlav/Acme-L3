f3d8a76ad8c44c2e45c4cb8aa9786e1a07e58263
JMGarCas <jmgarciacasillas135@gmail.com>
2023-04-09 09:44:26 +0200

    feat: Student 3 FR13 (Incomplete)

---
+++ Acme-L3-D02/src/main/java/acme/features/authenticated/tutorial/AuthenticatedTutorialListService.java
@@ -0,0 +1,61 @@
+
+package acme.features.authenticated.tutorial;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.Tutorial;
+import acme.framework.components.accounts.Authenticated;
+import acme.framework.components.models.Tuple;
+import acme.framework.services.AbstractService;
+
+@Service
+public class AuthenticatedTutorialListService extends AbstractService<Authenticated, Tutorial> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AuthenticatedTutorialRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void check() {
+		boolean status;
+
+		status = super.getRequest().hasData("courseId", int.class);
+
+		super.getResponse().setChecked(status);
+	}
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<Tutorial> object;
+		int courseId;
+
+		courseId = super.getRequest().getData("courseId", int.class);
+		object = this.repository.findTutorialByCourse(courseId);
+
+		super.getBuffer().setData(object);
+	}
+
+	@Override
+	public void unbind(final Tutorial object) {
+		assert object != null;
+
+		Tuple tuple;
+
+		tuple = super.unbind(object, "code", "title");
+
+		super.getResponse().setData(tuple);
+	}
+
+}
